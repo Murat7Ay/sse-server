e109000e4fccef843edc778babd56dde0a3dec1c
Murat Ay <murataybtp@gmail.com>
2022-09-04 02:50:05 +0300

    Update server.js
    translate destegi eklendi.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const https = require('https');
 const app = express();
 
 app.use(cors());
@@ -24,6 +24,9 @@ app.listen(PORT, () => {
 })
 
 function eventsHandler(request, response, next) {
+    if (clients.length > 5) {
+        throw new Error("Maximum client exceed");
+    }
     const headers = {
         'Content-Type': 'text/event-stream',
         'Connection': 'keep-alive',
@@ -33,7 +36,6 @@ function eventsHandler(request, response, next) {
     const clientId = request.query.id;
 
     response.writeHead(200, headers);
-
     if (clients.findIndex(x => x.id === clientId) < 0) {
         const newClient = {
             id: clientId,
@@ -53,13 +55,49 @@ app.get('/events', eventsHandler);
 
 
 function sendEventsToAll(newFact) {
-    clients.forEach(client => client.response.write(`data: ${JSON.stringify(newFact)}\n\n`))
+
+	let message = newFact.message;
+    var promise1 = translateRequest(message, 'tr-TR');
+    var promise2 = translateRequest(message, 'en-US');
+    var promise3 = translateRequest(message, 'ru-RU');
+
+    Promise.all([promise1, promise2, promise3]).then((values) => {
+    	let translateObject = values.reduce((obj, item) => Object.assign(obj, { [item.targetLanguage]: item.translateText }), {});
+    	newFact.translateResult = translateObject;
+        console.log(newFact);
+    	clients.forEach(client => client.response.write(`data: ${JSON.stringify(newFact)}\n\n`))
+    });
 }
 
 async function addFact(request, respsonse, next) {
     const newFact = request.body;
+    console.log(newFact)
     respsonse.json(newFact)
     return sendEventsToAll(newFact);
 }
 
 app.post('/fact', addFact);
+
+
+
+
+function translateRequest(message, tl) {
+    return new Promise((resolve, reject) => {
+        https.get('https://clients5.google.com/translate_a/t?client=dict-chrome-ex&sl=auto&tl=' + tl + '&q=' + encodeURI(message), (response) => {
+            let chunks_of_data = [];
+            response.on('data', (fragments) => {
+                chunks_of_data.push(fragments);
+            });
+            response.on('end', () => {
+                let response_body = Buffer.concat(chunks_of_data);
+                let response_body_json= JSON.parse(response_body.toString());
+                let translateText = response_body_json[0][0];
+                let targetLanguage = tl;
+                resolve({translateText,targetLanguage});
+            });
+            response.on('error', (error) => {
+                reject(error);
+            });
+        });
+    });
+}
