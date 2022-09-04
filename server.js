6d203f471aa94888d9e7fd60afb9a0a916a8b5cc
Murat Ay <murataybtp@gmail.com>
2022-09-04 23:49:52 +0300

    update server.js
    Chat odası şifrelerine göre haberleşme

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({
 
 app.get('/status', (request, response) => response.json({
     clients: clients.length,
-    details: clients.map(s => s.id)
+    details: clients.map(s => s.settings)
 }));
 
 const PORT = process.env.PORT || 3000;
@@ -23,22 +23,29 @@ app.listen(PORT, () => {
     console.log(`Facts Events service listening at http://localhost:${PORT}`)
 })
 
+app.get('/events', eventsHandler);
 function eventsHandler(request, response, next) {
     if (clients.length > 5) {
-        throw new Error("Maximum client exceed");
+        response.writeHead(418, "Maximum client exceed");
+        return;
     }
     const headers = {
         'Content-Type': 'text/event-stream',
         'Connection': 'keep-alive',
         'Cache-Control': 'no-cache'
     };
-
+    let settings = {
+        nickName: request.query.nickName,
+        avatarNo: request.query.avatarNo,
+        chatPassword: request.query.chatPassword
+    };
     const clientId = request.query.id;
 
     response.writeHead(200, headers);
     if (clients.findIndex(x => x.id === clientId) < 0) {
         const newClient = {
             id: clientId,
+            settings,
             response
         };
 
@@ -51,24 +58,8 @@ function eventsHandler(request, response, next) {
     });
 }
 
-app.get('/events', eventsHandler);
 
-
-function sendEventsToAll(newFact) {
-
-	let message = newFact.message;
-    var promise1 = translateRequest(message, 'tr-TR');
-    var promise2 = translateRequest(message, 'en-US');
-    var promise3 = translateRequest(message, 'ru-RU');
-
-    Promise.all([promise1, promise2, promise3]).then((values) => {
-    	let translateObject = values.reduce((obj, item) => Object.assign(obj, { [item.targetLanguage]: item.translateText }), {});
-    	newFact.translateResult = translateObject;
-        console.log(newFact);
-    	clients.forEach(client => client.response.write(`data: ${JSON.stringify(newFact)}\n\n`))
-    });
-}
-
+app.post('/fact', addFact);
 async function addFact(request, respsonse, next) {
     const newFact = request.body;
     console.log(newFact)
@@ -76,8 +67,20 @@ async function addFact(request, respsonse, next) {
     return sendEventsToAll(newFact);
 }
 
-app.post('/fact', addFact);
-
+function sendEventsToAll(newFact) {
+    let message = newFact.message;
+    const usersWithSamePassword = clients.filter(function (e) {
+        return e.settings.chatPassword === newFact.settings.chatPassword;
+    });
+    var promise1 = translateRequest(message, 'tr-TR');
+    var promise2 = translateRequest(message, 'en-US');
+    var promise3 = translateRequest(message, 'ru-RU');
+    Promise.all([promise1, promise2, promise3]).then((values) => {
+        let translateObject = values.reduce((obj, item) => Object.assign(obj, { [item.targetLanguage]: item.translateText }), {});
+        newFact.translateResult = translateObject;
+        usersWithSamePassword.forEach(client => client.response.write(`data: ${JSON.stringify(newFact)}\n\n`));
+    });
+}
 
 
 
@@ -90,10 +93,10 @@ function translateRequest(message, tl) {
             });
             response.on('end', () => {
                 let response_body = Buffer.concat(chunks_of_data);
-                let response_body_json= JSON.parse(response_body.toString());
+                let response_body_json = JSON.parse(response_body.toString());
                 let translateText = response_body_json[0][0];
                 let targetLanguage = tl;
-                resolve({translateText,targetLanguage});
+                resolve({ translateText, targetLanguage });
             });
             response.on('error', (error) => {
                 reject(error);
